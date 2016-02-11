3dde3264bda764c6ec8f8d60ab1aa757d391e3b6
tunnckoCore <mameto_100@mail.bg>
2016-02-11 06:47:32 +0200

    license year range

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /*!
  * abbrev-kindof <https://github.com/tunnckoCore/abbrev-kindof>
  *
- * Copyright (c) 2015 Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
+ * Copyright (c) 2015-2016 Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
  * Released under the MIT license.
  */
 
